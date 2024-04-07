04ec9c0159709c70a69153919ac2f5bc6b33e159
Kevdieker <kevin.kerbl.kk@gmail.com>
2024-04-07 17:50:29 +0200

    Implemented movie trailer feature

--- app/src/main/java/com/example/movieappmad24/screens/DetailScreen.kt
+++ app/src/main/java/com/example/movieappmad24/screens/DetailScreen.kt
@@ -7,21 +7,25 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import androidx.media3.exoplayer.ExoPlayer
 
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import com.example.movieappmad24.widget.MovieCard
 import com.example.movieappmad24.models.getMovies
 import com.example.movieappmad24.viewmodels.MoviesViewModel
+import com.example.movieappmad24.widget.Player
 import com.example.movieappmad24.widget.SimpleTopAppBar
 
 @Composable
 fun DetailScreen(navController: NavController, movieId: String?,moviesViewModel: MoviesViewModel) {
     val movie = moviesViewModel.movies.find { it.id == movieId }
+
     Scaffold(
         topBar = {
             SimpleTopAppBar(
@@ -33,12 +37,14 @@ fun DetailScreen(navController: NavController, movieId: String?,moviesViewModel:
         Column(modifier = Modifier.padding(innerPadding)) {
             movie?.let { safeMovie ->
                 MovieCard(movie = safeMovie)
+                Text(text = "Movie Trailer")
+                Player()
                 LazyRow(modifier = Modifier.padding(8.dp)) {
                     items(safeMovie.images.drop(1)) { image ->
                         Card(modifier = Modifier.padding(end = 8.dp)) {
                             AsyncImage(
                                 model = image,
-                                modifier = Modifier.size(128.dp),
+                                modifier = Modifier.size(178.dp),
                                 contentScale = ContentScale.Crop,
                                 contentDescription = "Movie Image"
                             )
